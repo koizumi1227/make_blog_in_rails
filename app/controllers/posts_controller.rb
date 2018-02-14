cf825e973b5a4fe5f5c98eba4ff89722ad164217
koizumi <fax1227@hotmail.co.jp>
2018-02-14 20:06:39 +0900

    ifでログイン状態のみ削除linkを表示変更。削除するにはログインを求める変更。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :new]
+  before_action :authenticate_user!, only: [:create, :new, :destroy]
 
   def new
     @post = Post.new
