b2a6cff7e6488a432014074406eeb6f2a51784b3
koizumi <fax1227@hotmail.co.jp>
2018-02-15 18:46:47 +0900

    日記への画像アップローダー実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :postimage)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  mount_uploader :postimage, PostimageUploader
+
 
   validates :title,  presence: true
   validates :content, presence: true
