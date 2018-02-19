3c85fd2ead1e43840434ac3ad36c54f19d3f0126
ms2sato <ms2.sato@gmail.com>
2018-02-19 18:34:17 +0900

    build and save

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.build(comment_params)
     if @comment.save
       redirect_to post_path(@post)
     else
