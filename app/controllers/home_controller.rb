98a0db9a340c8aa77603aee23d7fd5f4641ddc9c
koizumi <fax1227@hotmail.co.jp>
2018-02-22 16:11:01 +0900

    ユーザーの投稿一覧表示実装。 編集と削除はmypageからのみに変更。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,11 @@ class HomeController < ApplicationController
   def index
     @post = Post.all.includes(:user).order('created_at DESC')
     # @post = Post.all.order('created_at DESC')
-    @user = current_user.username if user_signed_in?
+    @user = current_user if user_signed_in?
   end
 
   def about
-    render 'layouts/about.html'
+    @user = current_user if user_signed_in?
+    render 'layouts/about'
   end
 end
