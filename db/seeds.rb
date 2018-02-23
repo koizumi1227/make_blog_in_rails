2d320fcc1134305b6e08eb9ab3bc491b67883a2e
koizumi <fax1227@hotmail.co.jp>
2018-02-23 17:24:56 +0900

    seedファイルのコメントアウト

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
 require 'faker'
-# user = User.new(username: "sampleuser",
-#                 email: "sample@sample.com",
-#                 password:              "111111",
-#                 password_confirmation: "111111",
-#                 confirmed_at: DateTime.now
-#                 )
-# user.skip_confirmation!
-# user.save!
-#
-# 5.times do |n|
-#   name  = "sampleuser#{n+1}"
-#   email = "sample#{n+1}@sample.com"
-#   password = "password"
-#   user = User.new(username: name,
-#                   email: email,
-#                   password:              password,
-#                   password_confirmation: password,
-#                   confirmed_at: DateTime.now
-#                   )
-#   user.skip_confirmation!
-#   user.save!
-# end
+user = User.new(username: "sampleuser",
+                email: "sample@sample.com",
+                password:              "111111",
+                password_confirmation: "111111",
+                confirmed_at: DateTime.now
+                )
+user.skip_confirmation!
+user.save!
+
+5.times do |n|
+  name  = "sampleuser#{n+1}"
+  email = "sample#{n+1}@sample.com"
+  password = "password"
+  user = User.new(username: name,
+                  email: email,
+                  password:              password,
+                  password_confirmation: password,
+                  confirmed_at: DateTime.now
+                  )
+  user.skip_confirmation!
+  user.save!
+end
 
 users = User.order(:created_at).take(3)
 3.times do
