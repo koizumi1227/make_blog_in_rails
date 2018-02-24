72369769b6995b1fa0856d68c5f471b2481507a6
koizumi <fax1227@hotmail.co.jp>
2018-02-24 15:16:46 +0900

    before_action 追加。不要なaction削除

--- app/controllers/manage_posts_controller.rb
+++ app/controllers/manage_posts_controller.rb
@@ -1,50 +1,21 @@
 class ManagePostsController < ApplicationController
-  # before_action :set_manage_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:index, :show, :edit, :update, :destroy]
 
-  # GET /manage_posts
-  # GET /manage_posts.json
   def index
       @user = current_user
       @user_post = current_user.posts.order('created_at DESC')
   end
 
-  # GET /manage_posts/1
-  # GET /manage_posts/1.json
   def show
     @user = current_user
-    # user/posts (ログイン状態でマイページからアクセスしたとき)
     @user_post = @user.posts.find(params[:id])
   end
 
-  # GET /manage_posts/new
-  def new
-    @manage_post = ManagePost.new
-  end
-
-  # GET /manage_posts/1/edit
   def edit
     @user = current_user
     @user_post = current_user.posts.find(params[:id])
   end
 
-  # POST /manage_posts
-  # POST /manage_posts.json
-  def create
-    @manage_post = ManagePost.new(manage_post_params)
-
-    respond_to do |format|
-      if @manage_post.save
-        format.html { redirect_to @manage_post, notice: 'Manage post was successfully created.' }
-        format.json { render :show, status: :created, location: @manage_post }
-      else
-        format.html { render :new }
-        format.json { render json: @manage_post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /manage_posts/1
-  # PATCH/PUT /manage_posts/1.json
   def update
     @user = current_user
     @user_post = Post.find(params[:id])
@@ -55,8 +26,6 @@ class ManagePostsController < ApplicationController
     end
   end
 
-  # DELETE /manage_posts/1
-  # DELETE /manage_posts/1.json
   def destroy
     user_post = Post.find(params[:id])
     user_post.destroy
